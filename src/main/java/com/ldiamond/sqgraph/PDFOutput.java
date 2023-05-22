817048d0918a3c6911e5332078587f66687f974d
Larry Diamond <1066589+larrydiamond@users.noreply.github.com>
2023-05-21 21:22:10 -0400

    upgraded openpdf to use pdfptable still not ready for prime time

--- src/main/java/com/ldiamond/sqgraph/PDFOutput.java
+++ src/main/java/com/ldiamond/sqgraph/PDFOutput.java
@@ -12,11 +12,15 @@ import com.google.common.collect.HashBasedTable;
 import com.lowagie.text.Cell;
 import com.lowagie.text.Document;
 import com.lowagie.text.Element;
+import com.lowagie.text.Font;
 import com.lowagie.text.Image;
 import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
 import com.lowagie.text.Rectangle;
 import com.lowagie.text.Table;
 import com.lowagie.text.alignment.HorizontalAlignment;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 
 public class PDFOutput {
@@ -88,8 +92,8 @@ public class PDFOutput {
         return width;
     }
 
-    private static void setMax (List<Integer> widths, int col, String s) {
-        int width = getWidthOfString(s);
+    private static void setMax (List<Integer> widths, int col, String s, int plus) {
+        int width = getWidthOfString(s) + plus;
         int curMax = widths.get (col);
         if (width > curMax) {
             widths.set (col, width);
@@ -97,47 +101,59 @@ public class PDFOutput {
     }
 
     public static void addTextDashboard(final HashBasedTable<String, String, Double> dashboardData, final Config config) {
+        document.add(new Phrase ("")); // spacer
+        Phrase p = new Phrase ("blah");
+        Font f = p.getFont();
+//        System.out.println ("Font " + f.getFamilyname() + " size " + f.getSize());
+
         List<Integer> colWidths = new ArrayList<>(); 
         
-        Table table = new Table(config.getMetrics().length + 1);
-        table.setWidth(100);
-        table.setPadding(3);
-        table.setSpacing(1);
-        Cell cell = new Cell("");
-        cell.setHeader(true);
+        PdfPTable table = new PdfPTable(config.getMetrics().length + 1);
+        table.setWidthPercentage(100);
+        table.setSpacingBefore(1);
+        table.setSpacingAfter(2);
+        PdfPCell cell = new PdfPCell(new Phrase ("Text"));
         table.addCell(cell);
         colWidths.add (2);
         int col = 1;
         for (SQMetrics m : config.getMetrics()) {
-            cell = new Cell(m.getTitle());
-            cell.setHorizontalAlignment(HorizontalAlignment.CENTER);
-            cell.setHeader(true);
+            cell = new PdfPCell(new Phrase (m.getTitle()));
+
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
             table.addCell(cell);
             colWidths.add(getWidthOfString (m.getTitle()));
             col++;
         }
-        table.endHeaders();
         for (Application a : config.getApplications()) {
             col = 0;
-            cell = new Cell(a.getTitle());
-            cell.setHorizontalAlignment(HorizontalAlignment.LEFT);
-            setMax(colWidths, col, a.getTitle());
+            cell = new PdfPCell(new Phrase (a.getTitle()));
+            cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+            cell.setVerticalAlignment(Element.ALIGN_CENTER);
+            setMax(colWidths, col, a.getTitle(), 0);
             table.addCell(cell);
             for (SQMetrics m : config.getMetrics()) {
                 col++;
                 String text = SqgraphApplication.standardDecimalFormatter.format (dashboardData.get(m.getTitle(),a.getTitle()));
-                cell = new Cell(text);
-                cell.setHorizontalAlignment(HorizontalAlignment.RIGHT);
+                cell = new PdfPCell(new Phrase (text));
+                cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+                cell.setVerticalAlignment(Element.ALIGN_CENTER);
+                cell.setPaddingLeft(cell.getPaddingLeft() + 2);
+                cell.setPaddingRight(cell.getPaddingRight() + 2);
                 table.addCell(cell);
-                setMax(colWidths, col, text);
+                setMax(colWidths, col, text, 4);
             }
         }
 
         int [] w = new int [colWidths.size()];
+        int sum = 0;
         for (int loop = 0; loop < colWidths.size(); loop++) {
-            System.out.println ("width " + loop + " = " + colWidths.get(loop));
+//            System.out.println ("width " + loop + " = " + colWidths.get(loop));
             w [loop] = colWidths.get(loop);
+            sum += w [loop];
         }
+//        System.out.println ("Sum = " + sum);
+
+        
 
         table.setWidths(w);
 
