ef853679d1979f7966683cfb420c899ddaeb9b3a
Larry Diamond <1066589+larrydiamond@users.noreply.github.com>
2023-08-09 21:54:26 -0400

    code coverage

---
+++ src/test/java/com/ldiamond/sqgraph/GraphOutputTests.java
@@ -0,0 +1,43 @@
+package com.ldiamond.sqgraph;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class GraphOutputTests {
+
+	@Test
+	void testAddSeriesForNativeMetric() {
+        List<Date> dates = new ArrayList<>();
+        List<Double> doubles = new ArrayList<>();
+        AssembledSearchHistory history = new AssembledSearchHistory();
+        List<Measures> measureList = new ArrayList<>();
+        history.setMeasures(measureList);
+        Measures m = new Measures();
+        measureList.add(m);
+        m.setMetric("unittest");
+        History[] historyArray = new History[2];
+        m.setHistory(historyArray);
+        History h0 = new History();
+        historyArray[0] = h0;
+        h0.setDate(new Date("Sat, 12 Aug 1995 13:30:00 GMT"));
+        h0.setValue(100.0);
+        History h1 = new History();
+        historyArray[1] = h1;
+        h1.setDate(new Date("Sat, 19 Aug 1995 13:30:00 GMT"));
+        h1.setValue(200.0);
+        Double lastPoint = GraphOutput.addSeriesForNativeMetric("unittest", history, dates, doubles);
+        assertEquals (200.0, lastPoint);
+		assertEquals (2, dates.size());
+		assertEquals ("Sat Aug 12 20:00:00 EDT 1995", dates.get(0).toString());
+		assertEquals ("Sat Aug 19 20:00:00 EDT 1995", dates.get(1).toString());
+		assertEquals (2, doubles.size());
+        assertEquals (100.0, doubles.get(0));
+        assertEquals (200.0, doubles.get(1));
+	}
+}
