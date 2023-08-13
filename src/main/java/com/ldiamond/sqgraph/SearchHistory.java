bec6cbe91291b7be1813bdb491acb986c91dc245
Larry Diamond <1066589+larrydiamond@users.noreply.github.com>
2023-08-12 20:44:48 -0400

    restoring full java 8 buildability until 1 jan 2024

--- src/main/java/com/ldiamond/sqgraph/SearchHistory.java
+++ src/main/java/com/ldiamond/sqgraph/SearchHistory.java
@@ -1,4 +1,13 @@
-// Copyright Larry Diamond 2023 All Rights Reserved
+/**
+ *    Copyright (C) 2023-present Larry Diamond, All Rights Reserved.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    license linked below for more details.
+ *
+ *    For license terms, see <https://github.com/larrydiamond/sqgraph/blob/main/LICENSE.md>.
+ **/
 package com.ldiamond.sqgraph;
 
 import java.util.Date;
--- src/test/java/com/ldiamond/sqgraph/ArchitectureUnitTests.java
+++ src/test/java/com/ldiamond/sqgraph/ArchitectureUnitTests.java
@@ -1,3 +1,13 @@
+/**
+ *    Copyright (C) 2023-present Larry Diamond, All Rights Reserved.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    license linked below for more details.
+ *
+ *    For license terms, see <https://github.com/larrydiamond/sqgraph/blob/main/LICENSE.md>.
+ **/
 package com.ldiamond.sqgraph;
 
 import com.tngtech.archunit.junit.AnalyzeClasses;
