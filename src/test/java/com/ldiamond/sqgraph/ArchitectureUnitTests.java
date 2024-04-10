e54866236b6520c49a7817b7b7713c65c74dd3f4
Larry Diamond <1066589+larrydiamond@users.noreply.github.com>
2024-04-10 18:08:59 -0400

    remove redundant architecture unit tests

--- src/test/java/com/ldiamond/sqgraph/ArchitectureUnitTests.java
+++ src/test/java/com/ldiamond/sqgraph/ArchitectureUnitTests.java
@@ -9,45 +9,14 @@
  *    For license terms, see <https://github.com/larrydiamond/sqgraph/blob/main/LICENSE.md>.
  **/
 package com.ldiamond.sqgraph;
-
-import com.tngtech.archunit.junit.AnalyzeClasses;
-import com.tngtech.archunit.junit.ArchTest;
-import com.tngtech.archunit.lang.ArchRule;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
-import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_USE_JODATIME;
-
-import java.util.Arrays;
-
 import org.junit.jupiter.api.Test;
+import java.util.Arrays;
 
 import com.ldiamond.archunittest.ArchitectureRule;
 import com.ldiamond.archunittest.ArchitectureUnitTest;
 
-@AnalyzeClasses(packages = "com.ldiamond.sqgraph")
 public class ArchitectureUnitTests {
-    
-    @ArchTest
-    static final ArchRule no_vectors = noClasses().should().callConstructor(java.util.Vector.class).because("Use ArrayList");
-    
-    @ArchTest
-    static final ArchRule no_hashtable = noClasses().should().callConstructor(java.util.Hashtable.class).because("Use HashSet");
-    
-    @ArchTest
-    static final ArchRule no_stringbuffer = noClasses().should().callConstructor(java.lang.StringBuffer.class).because("Use StringBuilder");
-    
-    @ArchTest
-    static final ArchRule no_treeset = noClasses().should().callConstructor(java.util.TreeSet.class).because("Use ConcurrentSkipListSet");
-
-    @ArchTest
-    static final ArchRule no_threadgroup = noClasses().should().callConstructor(java.lang.ThreadGroup.class).because("Use ExecutorService");
-
-    @ArchTest
-    static final ArchRule no_treemap = noClasses().should().callConstructor(java.util.TreeMap.class).because("Use ConcurrentSkipListMap");
-
-    @ArchTest
-    private final ArchRule no_jodatime = NO_CLASSES_SHOULD_USE_JODATIME;
-
-    
+       
     @Test
     public void runArchitectureTests() {
         ArchitectureUnitTest.testArchitecture(
