f206ae892b6511161dcb032ab12f9cd5c8e0694f
Larry Diamond <ldiamond@ldiamond.com>
2025-10-12 14:19:09 -0400

    prepare for junit 6

--- src/test/java/com/ldiamond/sqgraph/ArchitectureUnitTests.java
+++ src/test/java/com/ldiamond/sqgraph/ArchitectureUnitTests.java
@@ -14,7 +14,16 @@ import java.util.Arrays;
 
 import com.ldiamond.archunittest.ArchitectureRule;
 import com.ldiamond.archunittest.ArchitectureUnitTest;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noFields;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noMethods;
+
+@AnalyzeClasses(packages = "com.ldiamond.sqgraph")
 public class ArchitectureUnitTests {
        
     @Test
@@ -23,5 +32,19 @@ public class ArchitectureUnitTests {
             Arrays.asList(ArchitectureRule.ARCHUNIT_DEPRECATED_API_SHOULD_NOT_BE_USED, ArchitectureRule.ARCHUNIT_NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS),
             "com.ldiamond.sqgraph");
     }
+
+//    @ArchTest
+//    static final ArchRule no_public_static_decimalformat =
+//            noFields().that().arePublic().and().areStatic().should().haveRawType("java.text.DecimalFormat").because("DecimalFormat aint threadsafe use java.text.NumberFormatter").allowEmptyShould(true);
+
+    @ArchTest
+    static final ArchRule simpledateformat =
+            noFields().that().arePublic().and().areStatic().should().haveRawType("java.text.SimpleDateFormat").because("SimpleDateFormat aint threadsafe use java.text.DateTimeFormatter").allowEmptyShould(true);
+
+
+    @ArchTest
+    static final ArchRule methods_with_the_async_annotation_must_be_public =
+            methods().that().areAnnotatedWith("org.springframework.scheduling.annotation.Async").should().bePublic()
+                    .because("Spring requires Async methods to be public").allowEmptyShould(true);
 
 }
