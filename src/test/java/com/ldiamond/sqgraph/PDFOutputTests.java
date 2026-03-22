1d2ec78fb03fa0954748cc12ee0c8804e32b2aaf
Larry Diamond <ldiamond@ldiamond.com>
2026-03-21 22:50:33 -0400

    creating direct tests for the set background color methods

--- src/test/java/com/ldiamond/sqgraph/PDFOutputTests.java
+++ src/test/java/com/ldiamond/sqgraph/PDFOutputTests.java
@@ -18,6 +18,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.eq;
 
 import java.awt.Color;
 import java.util.ArrayList;
@@ -227,6 +228,48 @@ class PDFOutputTests {
         Color bg = numericCell.getBackgroundColor();
         assertNotNull(bg);
         assertEquals(Color.YELLOW, bg);
+    }
+
+    @Test
+    void testSetBackgroundColorForCellGreenHigher_green() {
+        final PdfPCell cell = mock(PdfPCell.class);
+        PDFOutput.setBackgroundColorForCellGreenHigher(80.0, 70.0, 50.0, cell);
+        verify(cell, times(1)).setBackgroundColor(eq(Color.GREEN));
+    }
+
+    @Test
+    void testSetBackgroundColorForCellGreenHigher_yellow() {
+        final PdfPCell cell = mock(PdfPCell.class);
+        PDFOutput.setBackgroundColorForCellGreenHigher(60.0, 70.0, 50.0, cell);
+        verify(cell, times(1)).setBackgroundColor(eq(Color.YELLOW));
+    }
+
+    @Test
+    void testSetBackgroundColorForCellGreenHigher_pink() {
+        final PdfPCell cell = mock(PdfPCell.class);
+        PDFOutput.setBackgroundColorForCellGreenHigher(40.0, 70.0, 50.0, cell);
+        verify(cell, times(1)).setBackgroundColor(eq(Color.PINK));
+    }
+
+    @Test
+    void testSetBackgroundColorForCellGreenLower_green() {
+        final PdfPCell cell = mock(PdfPCell.class);
+        PDFOutput.setBackgroundColorForCellGreenLower(40.0, 50.0, 70.0, cell);
+        verify(cell, times(1)).setBackgroundColor(eq(Color.GREEN));
+    }
+
+    @Test
+    void testSetBackgroundColorForCellGreenLower_yellow() {
+        final PdfPCell cell = mock(PdfPCell.class);
+        PDFOutput.setBackgroundColorForCellGreenLower(60.0, 50.0, 70.0, cell);
+        verify(cell, times(1)).setBackgroundColor(eq(Color.YELLOW));
+    }
+
+    @Test
+    void testSetBackgroundColorForCellGreenLower_pink() {
+        final PdfPCell cell = mock(PdfPCell.class);
+        PDFOutput.setBackgroundColorForCellGreenLower(80.0, 50.0, 70.0, cell);
+        verify(cell, times(1)).setBackgroundColor(eq(Color.PINK));
     }
 
     @Test
